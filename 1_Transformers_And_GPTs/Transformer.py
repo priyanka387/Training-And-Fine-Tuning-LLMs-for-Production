6cf870e5ee626dbf77c3f7d17d9bfcd7d01e24af
priyanka387 <priyankapathak222@gmail.com>
2023-12-20 23:51:53 +0530

    Transformers

---
+++ 1_Transformers_And_GPTs/Transformer.py
@@ -0,0 +1,16 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+OPT = AutoModelForCausalLM.from_pretrained("facebook/opt-1.3b", load_in_8bit=True)
+tokenizer = AutoTokenizer.from_pretrained("facebook/opt-1.3b")
+
+inp = "The quick brown fox jumps over the lazy dog"
+inp_tokenized = tokenizer(inp, return_tensors="pt")
+print(inp_tokenized['input_ids'].size())
+print(inp_tokenized)
+
+print(OPT.model)
+
+
+
+
+
