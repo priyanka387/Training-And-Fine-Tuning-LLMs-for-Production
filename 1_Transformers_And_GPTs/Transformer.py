1dc5fc39543e10c8370e7d1b7f786cbf7a0fac72
priyanka387 <priyankapathak222@gmail.com>
2023-12-21 23:28:14 +0530

    Transfer Architecture

--- 1_Transformers_And_GPTs/Transformer.py
+++ 1_Transformers_And_GPTs/Transformer.py
@@ -1,16 +1,12 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-OPT = AutoModelForCausalLM.from_pretrained("facebook/opt-1.3b", load_in_8bit=True)
-tokenizer = AutoTokenizer.from_pretrained("facebook/opt-1.3b")
+OPT = AutoModelForCausalLM.from_pretrained("facebook/opt-1.3b", load_in_8bit=True, quantize_on_cpu=True)
+tokenizer = AutoTokenizer.from_pretrained("facebook/opt-1.3b", quantize_on_cpu=True)
 
 inp = "The quick brown fox jumps over the lazy dog"
 inp_tokenized = tokenizer(inp, return_tensors="pt")
 print(inp_tokenized['input_ids'].size())
 print(inp_tokenized)
-
-print(OPT.model)
-
-
 
 
 
